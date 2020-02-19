bde681e4ddab59b7742f0d1b89af8d797edbd5d9
sigfried <rssanjuan2704@gmail.com>
2020-02-18 22:13:53 -0600

    refactor(feat): Separate constant values into different files

---
+++ modules/constants/colors.js
@@ -0,0 +1,8 @@
+const PALETTE = [
+  "#000000", "#ffffff", "#880000", "#aaffee",
+  "#cc44cc", "#00cc55", "#0000aa", "#eeee77",
+  "#dd8855", "#664400", "#ff7777", "#333333",
+  "#777777", "#aaff66", "#0088ff", "#bbbbbb"
+];
+
+export default PALETTE;
